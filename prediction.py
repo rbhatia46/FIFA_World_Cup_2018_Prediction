bc95f9c1e9870a6b77c52a97dd15639c12639377
Rahul Bhatia <rbhatia46@gmail.com>
2018-06-01 04:42:48 +0530

    Initial commit

---
+++ prediction.py
@@ -0,0 +1,143 @@
+# Import packages
+import numpy as np
+import pandas as pd
+
+# Loading the datasets
+wc = pd.read_csv('World_Cup_2018 Dataset.csv')
+results = pd.read_csv('results.csv')
+ranking = pd.read_csv('fifa_rankings.csv')
+fixtures = pd.read_csv('fixtures.csv')
+
+
+# Adding new column for winner of each match
+winner = []
+for i in range(len(results['home_team'])):
+    if results['home_score'][i] > results['away_score'][i]:
+        winner.append(results['home_team'][i])
+    elif results['home_score'][i] < results['away_score'][i]:
+        winner.append(results['away_team'][i])
+    else:
+        winner.append('Tie')
+results['winning_team'] = winner
+
+# Adding new column for goal difference in matches
+results['goal_difference'] = np.absolute(results['home_score'] - results['away_score'])
+
+
+# Create a DataFrame with all participating teams
+wc_teams = ['Australia', ' Iran', 'Japan', 'Korea Republic', 
+            'Saudi Arabia', 'Egypt', 'Morocco', 'Nigeria', 
+            'Senegal', 'Tunisia', 'Costa Rica', 'Mexico', 
+            'Panama', 'Argentina', 'Brazil', 'Colombia', 
+            'Peru', 'Uruguay', 'Belgium', 'Croatia', 
+            'Denmark', 'England', 'France', 'Germany', 
+            'Iceland', 'Poland', 'Portugal', 'Russia', 
+            'Serbia', 'Spain', 'Sweden', 'Switzerland']
+
+# Filter the 'results' dataframe to show only teams in this years' world cup, from 1930 onwards
+df_teams_home = results[results['home_team'].isin(wc_teams)]
+df_teams_away = results[results['away_team'].isin(wc_teams)]
+df_teams = pd.concat((df_teams_home, df_teams_away))
+df_teams.drop_duplicates()
+
+# Loop for creating a new column 'year'
+year = []
+for row in df_teams['date']:
+    year.append(int(row[:4]))
+df_teams['match_year'] = year
+
+# Slicing the dataset to see how many matches took place from 1930 onwards (first World Cup in 1930)
+df_teams30 = df_teams[df_teams.match_year >= 1930]
+df_teams30 = df_teams30.drop(['date', 'home_score', 'away_score', 'tournament', 'city', 'country', 'goal_difference', 'match_year'], axis=1)
+
+df_teams30 = df_teams30.reset_index(drop=True)
+df_teams30.loc[df_teams30.winning_team == df_teams30.home_team, 'winning_team']= 2
+df_teams30.loc[df_teams30.winning_team == 'Tie', 'winning_team']= 1
+df_teams30.loc[df_teams30.winning_team == df_teams30.away_team, 'winning_team']= 0
+
+from sklearn.model_selection import train_test_split
+
+# Get dummy variables
+final = pd.get_dummies(df_teams30, prefix=['home_team', 'away_team'], columns=['home_team', 'away_team'])
+
+# Separate X and y sets
+X = final.drop(['winning_team'], axis=1)
+y = final["winning_team"]
+y = y.astype('int')
+
+# Split train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+score = logreg.score(X_train, y_train)
+score2 = logreg.score(X_test, y_test)
+
+print("Training set accuracy: ", '%.3f'%(score))
+print("Test set accuracy: ", '%.3f'%(score2))
+
+
+# List for storing the group stage games
+pred_set = []
+
+# Create new columns with ranking position of each team
+fixtures.insert(1, 'first_position', fixtures['Home Team'].map(ranking.set_index('Team')['Position']))
+fixtures.insert(2, 'second_position', fixtures['Away Team'].map(ranking.set_index('Team')['Position']))
+
+# We only need the group stage games, so we have to slice the dataset
+fixtures = fixtures.iloc[:48, :]
+
+# Loop to add teams to new prediction dataset based on the ranking position of each team
+for index, row in fixtures.iterrows():
+    if row['first_position'] < row['second_position']:
+        pred_set.append({'home_team': row['Home Team'], 'away_team': row['Away Team'], 'winning_team': None})
+    else:
+        pred_set.append({'home_team': row['Away Team'], 'away_team': row['Home Team'], 'winning_team': None})
+        
+pred_set = pd.DataFrame(pred_set)
+backup_pred_set = pred_set
+
+# Get dummy variables and drop winning_team column
+pred_set = pd.get_dummies(pred_set, prefix=['home_team', 'away_team'], columns=['home_team', 'away_team'])
+
+# Add missing columns compared to the model's training dataset
+missing_cols = set(final.columns) - set(pred_set.columns)
+for c in missing_cols:
+    pred_set[c] = 0
+pred_set = pred_set[final.columns]
+
+# Remove winning team column
+pred_set = pred_set.drop(['winning_team'], axis=1)
+
+predictions = logreg.predict(pred_set)
+for i in range(fixtures.shape[0]):
+    print(backup_pred_set.iloc[i, 1] + " and " + backup_pred_set.iloc[i, 0])
+    if predictions[i] == 2:
+        print("Winner: " + backup_pred_set.iloc[i, 1])
+    elif predictions[i] == 1:
+        print("Tie")
+    elif predictions[i] == 0:
+        print("Winner: " + backup_pred_set.iloc[i, 0])
+    print('Probability of ' + backup_pred_set.iloc[i, 1] + ' winning: ', '%.3f'%(logreg.predict_proba(pred_set)[i][2]))
+    print('Probability of Tie: ', '%.3f'%(logreg.predict_proba(pred_set)[i][1]))
+    print('Probability of ' + backup_pred_set.iloc[i, 0] + ' winning: ', '%.3f'%(logreg.predict_proba(pred_set)[i][0]))
+    print("")
+    print("The Predicted Round of 16 teams thus for this year are : ")
+    print("Uruguay")
+    print("Spain")
+    print("Argentina")
+    print("Portugal")
+    print("Russia")
+    print("France")
+    print("Croatia")
+    print("Argentina")
+    print("Peru")
+    print("Brazil")
+    print("Mexico")
+    print("Germany")
+    print("Switzerland")
+    print("England")
+    print("Colombia")
+    print("Poland")
+    print("Belgium")
